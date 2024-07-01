9ac2894aa3329c181e63d877f5ec4e317adc05cb
Harishkumar0334 <jkharish0334@gmail.com>
2024-07-01 19:50:06 +0530

    Create modules.rb

---
+++ modules.rb
@@ -0,0 +1,27 @@
+#classrom
+  class Classroom
+    attr_accessor :student_count
+    attr_accessor :section
+    def initialize
+      @student_count = 0
+      @section = 'A'
+    end
+  
+    def attendance
+      @student_count = 40
+    end
+  
+    def self.school_name
+      "Ruby Rails School"
+    end
+  end
+  
+  a = Classroom.new
+  
+  puts a.attendance
+  
+  puts Classroom.school_name
+  
+  
+
+  
